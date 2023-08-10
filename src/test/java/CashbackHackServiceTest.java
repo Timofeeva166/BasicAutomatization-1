03372859b66352552aa179c371a04c3a0396cc48
Julia Timofeeva <J-Timofeeva166@yandex.ru>
2023-08-10 18:09:19 +0300

    TestNG

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,16 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+
+        Assert.assertEquals(service.remain(900), 100);
+        Assert.assertEquals(service.remain(0), 1000);
+        Assert.assertEquals(service.remain(999), 1);
+        Assert.assertEquals(service.remain(1000), 0);
+    }
+}
